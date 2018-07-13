a71ca0b840e79c3c7eb9ba0e124df9067cd7f111
sunxiaojiao <sunxiaojiao001@gmail.com>
2018-07-13 11:35:51 +0800

    code format

--- code/di/DI.php
+++ code/di/DI.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DI;
 
 require 'vendor/autoload.php';
@@ -10,50 +11,55 @@ use DI\Exceptions\ContainerException;
 
 class DI implements ContainerInterface
 {
-	// 存储对象
-	protected static $instance = [];
-	// 存储注册的依赖 
-	protected $mapping = [];
-	
-	// 绑定依赖
-    public function bind ($string, $value) {
+    // 存储对象
+    protected static $instance = [];
+    // 存储注册的依赖
+    protected $mapping = [];
+
+    // 绑定依赖
+    public function bind($string, $value)
+    {
         return $this->mapping[$string] = $value;
     }
 
-    public function instance ($string, $instance) {
+    public function instance($string, $instance)
+    {
         return self::$instance[$string] = $instance;
     }
-    
-    public function resolve ($string) {
+
+    public function resolve($string)
+    {
         if (!$this->has($string)) throw new NotFoundException;
         return $this->build($string);
     }
-    
-	// 构建对象
-	protected function build ($string) {
+
+    // 构建对象
+    protected function build($string)
+    {
         if (!$this->has($string)) {
             throw new NotFoundException;
         }
-        
+
         if (isset(self::$instance[$string])) {
             return self::$instance[$string];
         }
-        
+
         $className = $this->mapping[$string];
-        
+
         if (!class_exists($className)) return $className;
 
-        $class = new \ReflectionClass($className);
-	    $dependencies = $this->resolveDependencies($class);
-	    $instance = $class->newInstanceArgs($dependencies);
-		return $instance;
-	}
+        $class        = new \ReflectionClass($className);
+        $dependencies = $this->resolveDependencies($class);
+        $instance     = $class->newInstanceArgs($dependencies);
+        return $instance;
+    }
 
-	// 处理依赖
-	protected function resolveDependencies (\ReflectionClass $class) {
+    // 处理依赖
+    protected function resolveDependencies(\ReflectionClass $class)
+    {
         $constructor = $class->getConstructor();
-		
-		$resolvedDependencies = [];
+
+        $resolvedDependencies = [];
         if ($constructor && $constructor->isPublic()) {
             $params = $constructor->getParameters();
             foreach ($params as $param) {
@@ -65,31 +71,35 @@ class DI implements ContainerInterface
                 }
             }
         } else {
-          
+
         }
         return $resolvedDependencies;
-	}
-	
-	protected function resolveNonClass (\ReflectionParameter $param) {
+    }
+
+    protected function resolveNonClass(\ReflectionParameter $param)
+    {
         if ($param->isDefaultValueAvailable()) {
             return $param->getDefaultValue();
         }
-        
+
         throw new Exception($param->name . '没有赋值');
-	}
-	
-	// 实现psr-11
-	public function get ($string) {
+    }
+
+    // 实现psr-11
+    public function get($string)
+    {
         if (isset(self::$instance[$string])) return self::$instance[$string];
 
         if ($this->has($string)) {
             return $this->resolve($string);
         }
         throw new NotFoundException;
-	}
-	// 实现psr-11
-	public function has ($string) {
+    }
+
+    // 实现psr-11
+    public function has($string)
+    {
         return isset(self::$instance[$string]) || isset($this->mapping[$string]);
-	}
-	
+    }
+
 }
